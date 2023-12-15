0c3b3239479106d95513affa22ff339108e74772
YuriyBeregovoy <rik0681@gmail.com>
2023-12-15 16:47:00 +0200

    Update SignupGoal.jsx

--- src/Pages/SignupGoal/SignupGoal.jsx
+++ src/Pages/SignupGoal/SignupGoal.jsx
@@ -1,7 +1,10 @@
 const SignUpGoal = () => {
   return (
     <>
-      <p>Welcome! SignUpGoal Page</p>
+      <img src="" alt=""></img>
+      <h1>Your goal</h1>
+      <h2>Choose a goal so that we can help you effectively</h2>
+      <a href="#">Back</a>
     </>
   );
 };
