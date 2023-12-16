9fc047ba7f023c878722b581d31dedf943f453ee
YuriyBeregovoy <rik0681@gmail.com>
2023-12-16 12:57:58 +0200

    Added the backlink to the SinupGoal.jsx

--- src/Pages/SignupGoal/SignupGoal.jsx
+++ src/Pages/SignupGoal/SignupGoal.jsx
@@ -1,13 +1,17 @@
 import GoalForm from '../../components/SignupGoal/GoalForm';
+import { Link } from 'react-router-dom';
+import { useRef } from 'react';
 
 const SignUpGoal = () => {
+  const backLinkLocationRef = useRef(location.state?.from ?? '/signup');
+
   return (
     <>
       <img src="" alt=""></img>
       <h1>Your goal</h1>
       <h2>Choose a goal so that we can help you effectively</h2>
       <GoalForm />
-      {/* <Link to={backLinkLocationRef.current}>Back</Link> */}
+      <Link to={backLinkLocationRef.current}>Back</Link>
     </>
   );
 };
