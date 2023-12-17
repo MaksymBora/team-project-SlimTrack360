a102faba95c77ecbec0741d2f54debb5ac41bf19
TetyanaDubchak <123674629+TetyanaDubchak@users.noreply.github.com>
2023-12-17 10:47:19 +0200

    add function for ten elements

--- src/components/Recommended/List/List.styled.js
+++ src/components/Recommended/List/List.styled.js
@@ -12,10 +12,6 @@ export const FoodList = styled.ul`
     max-width: 676px;
   }
 `;
-export const Image = styled.img`
-  width: 46px;
-  height: 46px;
-`;
 
 export const Item = styled.li`
   background-color: ${theme.color.secondaryGrey};
@@ -29,26 +25,4 @@ export const Item = styled.li`
     width: calc((100% - 20px) / 2);
     margin-bottom: 0px;
   }
-`;
-
-export const InnerItemWrap = styled.div`
-  display: flex;
-  gap: 6px;
-`;
-
-export const FoodName = styled.h3`
-  font-size: 18px;
-  line-height: 24px;
-  margin-bottom: 2px;
-`;
-
-export const Text = styled.p`
-  color: ${theme.color.primaryGrey};
-  font-size: 14px;
-  line-height: 20px;
-`;
-
-export const Amount = styled.p`
-  font-size: 14px;
-  line-height: 20px;
 `;
