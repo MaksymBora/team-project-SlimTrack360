34402d7671b5361fab870d111e182f9dbf4cc808
MaksymBora <maxboraod@gmail.com>
2023-12-20 07:27:21 +0100

    fix foute

--- src/components/ForgotPass/ForgotPassContent.jsx
+++ src/components/ForgotPass/ForgotPassContent.jsx
@@ -13,9 +13,21 @@ import {
   Wrapper,
 } from '../Auth/Signup/SignUpContent.styled';
 import { Options, Label } from './ForgotPassContent.styled';
-import { basicSchema } from '../Auth/SignUp/schemas';
 import { LinkSignup } from '../Auth/Signin/SignInContent.styled';
+import * as yup from 'yup';
 
+const basicSchema = yup.object().shape({
+  name: yup.string().required('Name is required*'),
+  email: yup
+    .string()
+    .email('Enter a valid Email*')
+    .required('Email is required*'),
+  password: yup
+    .string()
+    .required('Password is required*')
+    .min(8, 'Password must be at least 8 characters long')
+    .max(64, 'Password must not exceed 64 characters'),
+});
 export const ForgotPassContent = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
