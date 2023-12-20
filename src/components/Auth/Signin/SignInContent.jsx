51259516c648044450a1c9180b688f68c87b7b15
MaksymBora <maxboraod@gmail.com>
2023-12-20 07:25:00 +0100

    fix route

--- src/components/Auth/Signin/SignInContent.jsx
+++ src/components/Auth/Signin/SignInContent.jsx
@@ -19,7 +19,7 @@ import { LinkSignup, Paragraph } from './SignInContent.styled';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { logIn } from '../../../Redux/userAuth/operations';
-import { basicSchema } from '../SignUp/schemas/index';
+import { basicSchema } from '../SignUp/schemas/index.js';
 
 export const SignInContent = () => {
   const [showPassword, setShowPassword] = useState(false);
