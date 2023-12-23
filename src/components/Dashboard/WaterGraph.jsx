d48a76596a8d83eb88ef759375c08522076e20c2
Galyna Shevchenko <galu4ok@gmail.com>
2023-12-24 01:52:45 +0200

    connecting the waterchart

--- src/components/Dashboard/WaterGraph.jsx
+++ src/components/Dashboard/WaterGraph.jsx
@@ -1,5 +1,5 @@
-// src\components\Dashboard\WaterGraph.jsx
-// import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectTotalWater } from '../../Redux/statisctics/selector';
 import { Line } from 'react-chartjs-2';
 import {
   commonOptions,
@@ -11,28 +11,67 @@ import './Styles/MobStyles/mob.Graph.css';
 import './Styles/TabletStyles/tab.Graph.css';
 import './Styles/Graph.css';
 
-const data = {
-  labels: Array.from({ length: 31 }, (_, i) => i + 1),
-  datasets: [
-    {
-      label: '',
-      data: Array.from({ length: 31 }, () => Math.floor(Math.random() * 1000)),
-      borderColor: '#e3ffa8',
-      borderWidth: 1,
-      pointBackgroundColor: '#e3ffa8',
-      pointRadius: 2,
-      fill: false,
-    },
-  ],
-};
-
 const WaterGraph = () => {
+  // Підписка на стор
+  const totalWater = useSelector(selectTotalWater);
+
+  if (!totalWater || totalWater.length === 0) {
+    return (
+      <GraphContainer className="scroll-container">
+        <div className="waterTitle">
+          <h2 className="graphTitle">Water</h2>
+          <h3 className="graphValue">No water consumption data available</h3>
+        </div>
+      </GraphContainer>
+    );
+  }
+  // Обчислення середньої кількості спожитої води
+  const averageWater = totalWater
+    ? Math.round(
+        totalWater.reduce(
+          (accumulator, current) => accumulator + current.water,
+          0
+        ) / totalWater.length
+      )
+    : 0;
+  // Отримання числової частини дати (від 1 до 31)
+  const extractDate = (dateString) => {
+    const dateObj = new Date(dateString);
+    return dateObj.getDate();
+  };
+  // Створення labels з числами від 1 до 31
+  const labels = Array.from({ length: 31 }, (_, i) => i + 1);
+
+  // Оновлений блок даних для графіка
+  const chartData = {
+    labels: labels.map((day) => day),
+    datasets: [
+      {
+        label: 'Water Intake',
+        data: labels.map((day) => {
+          const waterIntake = totalWater.find(
+            (item) => extractDate(item.date) === day
+          );
+          return waterIntake ? waterIntake.water : 0; // Заміна null на 0
+        }),
+        borderColor: '#e3ffa8',
+        borderWidth: 1,
+        pointBackgroundColor: '#e3ffa8',
+        pointRadius: 2,
+        fill: false,
+      },
+    ],
+  };
+
   return (
     <GraphContainer className="scroll-container">
       <div className="waterTitle">
         <h2 className="graphTitle">Water</h2>
         <h3 className="graphValue">
-          Average value: <span className="waterValue">1700 ml</span>
+          Average value:
+          <span className="waterValue">
+            {!totalWater ? `0 ml` : `${averageWater || 0} ml`}
+          </span>
         </h3>
       </div>
       <ChartContainer className="graph-line">
@@ -41,7 +80,7 @@ const WaterGraph = () => {
             ...commonOptions,
             scales: { x: commonXAxisOptions, y: waterYAxisOptions },
           }}
-          data={data}
+          data={chartData}
         />
       </ChartContainer>
     </GraphContainer>
