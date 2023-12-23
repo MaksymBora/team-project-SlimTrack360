c3eec114be5433e9a155c97e319c08fa855f671c
MaksymBora <123206568+MaksymBora@users.noreply.github.com>
2023-12-23 05:11:05 +0000

    Deploying to gh-pages from @ MaksymBora/team-project-SlimTrack360@18952ebd31ba9a70a880c881cc71e0915c0f6f3e 🚀

--- assets/SignInContent.styled-5200f05d.js
+++ assets/SignInContent.styled-5200f05d.js
@@ -1,4 +1,4 @@
-import{u as n,t as i}from"./index-c3227414.js";const p=n.p`
+import{u as n,t as i}from"./index-2c875308.js";const p=n.p`
   color: ${i.color.primaryGrey};
   text-align: center;
   font-size: 14px;
