7d3334edd7467c51e07c0c8d7fd0491dcb177453
MaksymBora <123206568+MaksymBora@users.noreply.github.com>
2023-12-25 17:53:54 +0000

    Deploying to gh-pages from @ MaksymBora/team-project-SlimTrack360@f73b598ace32eb8c6ee17b590f1afb37d53f4485 🚀

--- assets/Recommented-0eab87fe.js
+++ assets/Recommented-0eab87fe.js
@@ -1,4 +1,4 @@
-import{u as t,t as r,e as i,j as e,a as s,r as p,p as c,q as x}from"./index-54e5c679.js";import{s as d,I as h,a as g,b as l}from"./Item-69503557.js";const w=t.ul`
+import{u as t,t as r,e as i,j as e,a as s,r as p,p as c,q as x}from"./index-8a383172.js";import{s as d,I as h,a as g,b as l}from"./Item-0bc41975.js";const w=t.ul`
   @media screen and (min-width: 834px) {
     display: flex;
     flex-wrap: wrap;
--- assets/SignInContent.styled-8c55f8b8.js
+++ assets/SignInContent.styled-8c55f8b8.js
@@ -1,4 +1,4 @@
-import{u as n,t as i}from"./index-54e5c679.js";const p=n.p`
+import{u as n,t as i}from"./index-8a383172.js";const p=n.p`
   color: ${i.color.primaryGrey};
   text-align: center;
   font-size: 14px;
