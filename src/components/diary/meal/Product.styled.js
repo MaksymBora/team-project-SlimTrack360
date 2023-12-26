ae44940feac3545e977b250c98d0f3e8cc03ed9a
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2023-12-26 04:07:24 +0100

    fix hover svg

--- src/components/diary/meal/Product.styled.js
+++ src/components/diary/meal/Product.styled.js
@@ -93,7 +93,6 @@ export const EditButton = styled.button`
   &:hover {
     color: ${theme.color.primaryWhite};
     animation: 1s ease-in-out infinite;
-    fill: ${theme.color.primaryWhite};
   }
   &:focus {
     color: ${theme.color.primaryGrey};
